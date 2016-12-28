d216f78b41c062b924358614d115200d7ca3ed8e
Ryan Stortz <ryan@withzombies.com>
2016-12-27 22:51:20 -0500

    Fix project name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Kitura-Session-Fluent",
+    name: "KituraSessionFluent",
     dependencies: [
       .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 1),
       .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 1),
